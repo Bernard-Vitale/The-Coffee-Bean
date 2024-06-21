4cc57e3c47be6595873ae0b54570cb2cb5411171
Bernard Vitale <126836061+Bernard-Vitale@users.noreply.github.com>
2024-06-20 20:50:40 -0400

    Updates

--- src/App.jsx
+++ src/App.jsx
@@ -34,12 +34,12 @@ function App() {
     <>
       <NavBar shoppingCart={shoppingCart} />
       <div className="MainBodyContainer">
-        <Routes>
-          <Route path="/The-Coffee-Bean/" element={<HomePage coffeeData={coffeeData} loading={loading} error={error} />} />
-          <Route path="/The-Coffee-Bean/shop" element={<ShopPage items={coffeeData} loading={loading} error={error} />} />
-          <Route path="/The-Coffee-Bean/cart" element={<CartPage shoppingCart={shoppingCart} setShoppingCart={setShoppingCart} />}></Route>
+        <Routes >
+          <Route path="/" element={<HomePage coffeeData={coffeeData} loading={loading} error={error} />} />
+          <Route path="/shop" element={<ShopPage items={coffeeData} loading={loading} error={error} />} />
+          <Route path="/cart" element={<CartPage shoppingCart={shoppingCart} setShoppingCart={setShoppingCart} />}></Route>
           <Route
-            path="/The-Coffee-Bean/item/:id"
+            path="/item/:id"
             element={
               <ItemPage coffeeData={coffeeData} loading={loading} error={error} shoppingCart={shoppingCart} setShoppingCart={setShoppingCart} />
             }
