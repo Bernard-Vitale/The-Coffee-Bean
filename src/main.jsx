92a5ac0449848abda15328155bc3027f5c5534af
Bernard Vitale <126836061+Bernard-Vitale@users.noreply.github.com>
2024-06-20 20:54:26 -0400

    Updates

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from './App.jsx'
 
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/The-Coffee-Bean">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
